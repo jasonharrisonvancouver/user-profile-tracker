fc1413c984ecb58720f0a508d1683b858fbef021
Penjat <theSpencerSymington@Gmail.com>
2019-01-30 21:11:17 -0800

    created view controllers

---
+++ Dinder/ViewControllers/BrowseViewController.swift
@@ -0,0 +1,22 @@
+//
+//  BrowseViewController.swift
+//  Dinder
+//
+//  Created by Spencer Symington on 2019-01-30.
+//  Copyright © 2019 Spencer Symington. All rights reserved.
+//
+
+import UIKit
+
+class BrowseViewController: UIViewController {
+
+    override func viewDidLoad() {
+      super.viewDidLoad()
+      print("loaded browse view controller")
+      
+    }
+    
+
+  
+
+}
---
+++ Dinder/ViewControllers/ProfileViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ProfileViewController.swift
+//  Dinder
+//
+//  Created by Spencer Symington on 2019-01-30.
+//  Copyright © 2019 Spencer Symington. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+      super.viewDidLoad()
+      print("loaded profile view controller")
+      
+    }
+    
+
+    
+
+}
